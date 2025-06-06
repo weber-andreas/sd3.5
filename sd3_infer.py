ddc485ee68d1058e0d2c496b7b1f1b80e28c1692
Andreas Weber <andreas.s.weber@outlook.com>
2025-06-06 15:20:14 +0200

    modified inference for sd3-large

--- sd3_infer.py
+++ sd3_infer.py
@@ -82,7 +82,9 @@ CLIPG_CONFIG = {
 class ClipG:
     def __init__(self, model_folder: str, device: str = "cpu"):
         with safe_open(
-            f"{model_folder}/clip_g.safetensors", framework="pt", device="cpu"
+            f"{model_folder}/text_encoders/clip_g.safetensors",
+            framework="pt",
+            device="cpu",
         ) as f:
             self.model = SDXLClipG(CLIPG_CONFIG, device=device, dtype=torch.float32)
             load_into(f, self.model.transformer, "", device, torch.float32)
@@ -100,7 +102,9 @@ CLIPL_CONFIG = {
 class ClipL:
     def __init__(self, model_folder: str):
         with safe_open(
-            f"{model_folder}/clip_l.safetensors", framework="pt", device="cpu"
+            f"{model_folder}/text_encoders/clip_l.safetensors",
+            framework="pt",
+            device="cpu",
         ) as f:
             self.model = SDClipModel(
                 layer="hidden",
@@ -126,7 +130,9 @@ T5_CONFIG = {
 class T5XXL:
     def __init__(self, model_folder: str, device: str = "cpu", dtype=torch.float32):
         with safe_open(
-            f"{model_folder}/t5xxl.safetensors", framework="pt", device="cpu"
+            f"{model_folder}/text_encoders/t5xxl_fp16.safetensors",
+            framework="pt",
+            device="cpu",
         ) as f:
             self.model = T5XXLModel(T5_CONFIG, device=device, dtype=dtype)
             load_into(f, self.model.transformer, "", device, dtype)
@@ -227,7 +233,7 @@ SEEDTYPE = "rand"
 # Actual model file path
 # MODEL = "models/sd3_medium.safetensors"
 # MODEL = "models/sd3.5_large_turbo.safetensors"
-MODEL = "models/sd3.5_large.safetensors"
+MODEL = "models/sd3-large/sd3.5_large.safetensors"
 # VAE model file path, or set None to use the same model file
 VAEFile = None  # "models/sd3_vae.safetensors"
 # Optional init image file path
